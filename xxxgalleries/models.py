66428839020f859b6721e83d70483ba7a61aae0b
Marcos Lopez <dev@scidentify.info>
2013-05-30 08:38:04 -0400

    base model tables  and descriptions

--- xxxgalleries/models.py
+++ xxxgalleries/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Galleries(models.Model):
+    """ Will consist of a gallery instance
+    Whether it be hosted, or local galler
+    
+    - if local gallery, overwrite target_link to 
+    an internal link since we will be hosting
+    the galleries content.
+    
+    - if hosted gallery, we log the target link
+    and the program type to which the target 
+    pertains to
+    """
+    pass
+
+class Providers(models.Model):
+    """ Lets keep track of the providers that
+    we will be dealing with.
+    
+    - name
+    - username
+    - password
+    - website (optional)
+    - login url (optional)
+    - ccbill (optional)
+    - notes
+    """
+    pass
+
+class ProgramTypes(models.Model):
+    """ Keep track of the program types for 
+    a certain provider. Program types are e.g:
+    $35 dollar pps, per signups, per free joins, etc
+    """
+    provider = models.ForeignKey("Providers")
+
