fdb3e99f7656d6e52458ca1e66d1800160a12a69
Marcos Lopez <dev@scidentify.info>
2013-07-12 19:34:05 -0400

    renamed demo project name

--- demo/demoproject/settings.py
+++ demo/demoproject/settings.py
@@ -97,10 +97,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'xxxgalleries.urls'
+ROOT_URLCONF = 'demoproject.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'xxxgalleries.wsgi.application'
+WSGI_APPLICATION = 'demoproject.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
